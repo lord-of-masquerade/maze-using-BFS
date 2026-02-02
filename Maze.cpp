7b786b3dc5989f408465e0ee05bb0195fe7902a9
lord-of-masquerade <kaithecosmic002@gmail.com>
2026-02-02 17:53:08 +0530

    change default to maze to user input maze

--- Maze.cpp
+++ Maze.cpp
@@ -5,15 +5,15 @@ using namespace std;
 struct cell{
     int x,y, dist;
 };
-void printMaze(int maze[5][5], int pathMark[5][5]) {
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+void printMaze(vector<vector<int>>& maze, vector<vector<int>>& pathMark,int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
 
-            if (pathMark[i][j] && !(i == 0 && j == 0) && !(i == 4 && j == 4))
+            if (pathMark[i][j] && !(i == 0 && j == 0) && !(i == n-1 && j == m-1))
                 cout << "+ ";
             else if (i == 0 && j == 0)
                 cout << "S ";
-            else if (i == 4 && j == 4)
+            else if (i == n-1 && j == m-1)
                 cout << "E ";
             else if (maze[i][j] == 1)
                 cout << "# ";
@@ -24,15 +24,20 @@ void printMaze(int maze[5][5], int pathMark[5][5]) {
     }
 }
 int main(){
-    int maze[5][5] = {
-        {0,0,1,0,0},
-        {0,0,1,0,1},
-        {1,0,0,0,0},
-        {0,1,1,1,0},
-        {0,0,0,0,0}
-    };
-    int visited[5][5]={0};
-    pair<int,int> parent[5][5];
+    int n,m;
+    cout<<"Enter rows and columns ";
+    cin>>n>>m;
+    vector<vector<int>> maze(n, vector<int>(m));
+    cout<<"Enter maze (0=Path,1=Wall): \n";
+
+    for(int i=0 ; i<n; i++){
+        for(int j=0;j<m;j++){
+            cin>>maze[i][j];
+        }
+    }
+
+    vector<vector<int>> visited(n, vector<int>(m,0));
+    vector<vector<pair<int,int>>> parent(n, vector<pair<int,int>>(m));
 
     int dx[4]={-1,1,0,0};
     int dy[4]={0,0,-1,1};
@@ -49,7 +54,7 @@ int main(){
         cell cur=q.front();
         q.pop();
         
-        if (cur.x==4 && cur.y==4){
+        if (cur.x==n-1 && cur.y==m-1){
             cout << "Shortest path length: " << cur.dist << endl;
             endx=cur.x;
             endy=cur.y;
@@ -58,7 +63,7 @@ int main(){
         for(int i=0;i<4;i++){
             int nx=cur.x + dx[i];
             int ny=cur.y + dy[i];
-            if (nx>=0 && ny>=0 && nx<5 && ny<5){
+            if (nx>=0 && ny>=0 && nx<n && ny<m){
                 if (maze[nx][ny]==0 && !visited[nx][ny]){
                     visited[nx][ny]=1;
                     parent[nx][ny]={cur.x,cur.y};
@@ -67,10 +72,10 @@ int main(){
             }
         }
     }
-    cout<< "Path: ";
+
+    vector<vector<int>> pathMark(n,vector<int>(m,0));
     vector<pair<int,int>> path;
-    
-    while (endx!=-1 && endy!=-1)
+     while (endx!=-1 && endy!=-1)
     {
         path.push_back({endx,endy});
         auto p=parent[endx][endy];
@@ -78,14 +83,12 @@ int main(){
         endy=p.second;
     }
 
-     int pathMark[5][5]={0};
-
-    for (auto cell:path){
-        pathMark[cell.first][cell.second] = 1;
-    }
+    for (auto c : path) {
+    pathMark[c.first][c.second] = 1;
+}
 
     cout << "\nMaze with shortest path:\n";
     cout << " Path taken shown with \"+\"\n";
-    printMaze(maze, pathMark);
+    printMaze(maze, pathMark , n, m);
     return 0;
 }
